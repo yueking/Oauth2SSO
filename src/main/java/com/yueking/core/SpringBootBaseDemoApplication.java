7d56a3a074beeb46d64071645ed6936eff564573
yuewu <yuekinger@gmail.com>
2021-12-09 13:20:33 +0800

    init

---
+++ src/main/java/com/yueking/core/SpringBootBaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yueking.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBaseDemoApplication.class, args);
+    }
+
+}
