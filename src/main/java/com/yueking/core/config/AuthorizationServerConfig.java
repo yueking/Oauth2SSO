f4cf5ec8f582bebb9cdcea13b93cbdb04d69da8b
yuewu <yuekinger@gmail.com>
2021-12-10 18:53:13 +0800

    密码模式&授权码模式

--- src/main/java/com/yueking/core/config/AuthorizationServerConfig.java
+++ src/main/java/com/yueking/core/config/AuthorizationServerConfig.java
@@ -1,10 +1,13 @@
 package com.yueking.core.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 import javax.annotation.Resource;
 
@@ -16,6 +19,24 @@ import javax.annotation.Resource;
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
     @Resource
     private PasswordEncoder passwordEncoder;
+
+    @Resource
+    private AuthenticationManager authenticationManager;
+
+    @Resource
+    private UserDetailsService myUserDetailsService;
+    /**
+     * 密码模式需要 重载方法
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(myUserDetailsService);
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         //1.实际项目一般不使用内存管理
@@ -29,7 +50,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                  * 授权类型
                  * 授权码模式 简化模式 密码模式 客户端模式
                  * authorization_code:授权码模式
+                 * password:密码模式
                  */
-                .authorizedGrantTypes("authorization_code");
+                .authorizedGrantTypes("authorization_code","password");
     }
 }
--- src/main/java/com/yueking/core/config/SecurityConfig.java
+++ src/main/java/com/yueking/core/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.yueking.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
 
 @Configuration
 @EnableWebSecurity
@@ -14,6 +16,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 密码模式需要配置的依赖
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Override
