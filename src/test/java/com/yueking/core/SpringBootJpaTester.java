32a319bd34e0bab4f36227573a6a96509426dfd2
yuewu <yuekinger@gmail.com>
2021-12-12 18:53:52 +0800

    集成jpa

--- src/test/java/com/yueking/core/SpringBootJpaTester.java
+++ src/test/java/com/yueking/core/SpringBootJpaTester.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import java.util.Optional;
 
 @SpringBootTest
 public class SpringBootJpaTester {
