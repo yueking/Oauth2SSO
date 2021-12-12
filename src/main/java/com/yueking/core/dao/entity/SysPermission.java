ee4e15380c37067da0534297a0c5d45bf8cf3428
yuewu <yuekinger@gmail.com>
2021-12-12 17:23:17 +0800

    集成jpa

--- src/main/java/com/yueking/core/dao/entity/SysPermission.java
+++ src/main/java/com/yueking/core/dao/entity/SysPermission.java
@@ -1,6 +1,8 @@
 package com.yueking.core.dao.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
@@ -11,10 +13,12 @@ import java.io.Serializable;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class SysPermission implements GrantedAuthority, Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GenericGenerator(name = "persistenceGenerator", strategy = "increment")
+    private Integer id;
     private String permName;
     private String permTag;
     private String permDesc;
--- src/test/java/com/yueking/core/SpringBootJpaTester.java
+++ src/test/java/com/yueking/core/SpringBootJpaTester.java
@@ -1,12 +1,23 @@
 package com.yueking.core;
 
+import com.yueking.core.dao.entity.SysPermission;
+import com.yueking.core.dao.repository.PermissionDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 public class SpringBootJpaTester {
+    @Resource
+    private PermissionDao permissionDao;
     @Test
     void jpa(){
         System.out.println("jpa...");
+        SysPermission permission = new SysPermission();
+        permission.setPermName("perm1");
+        permission.setPermTag("perm1Tag");
+        permission.setPermDesc("perm1Desc");
+        permissionDao.saveAndFlush(permission);
     }
 }
