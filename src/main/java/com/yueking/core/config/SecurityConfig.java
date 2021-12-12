88ba149ffbdc79f3e9871b84bec990e2e98b1c88
yuewu <yuekinger@gmail.com>
2021-12-12 15:20:48 +0800

    basic表单认证

--- src/main/java/com/yueking/core/config/SecurityConfig.java
+++ src/main/java/com/yueking/core/config/SecurityConfig.java
@@ -18,16 +18,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password(passwordEncoder().encode("123"))
-                .authorities("/");
+                .authorities("addMember","delMember","updateMember","showMember");
+        auth.inMemoryAuthentication().withUser("add").password(passwordEncoder().encode("add")).authorities("addMember");
+        auth.inMemoryAuthentication().withUser("del").password(passwordEncoder().encode("del")).authorities("delMember");
+        auth.inMemoryAuthentication().withUser("update").password(passwordEncoder().encode("update")).authorities("updateMember");
+        auth.inMemoryAuthentication().withUser("show").password(passwordEncoder().encode("show")).authorities("showMember");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //配置认证方式 token 表单 basic模式
         http.authorizeRequests()
+                .antMatchers("/addMember").hasAuthority("addMember")
+                .antMatchers("/delMember").hasAuthority("delMember")
+                .antMatchers("/updateMember").hasAuthority("updateMember")
+                .antMatchers("/showMember").hasAuthority("showMember")
                 .antMatchers("/**")
                 .fullyAuthenticated().and()
-                .httpBasic();
+                // 基础模式
+                // .httpBasic();
+                // 表单模式
+                .formLogin();
     }
 
     @Bean
