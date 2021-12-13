6d1e3ad7702e55106233cb6d0d0912308bff1a67
yuewu <yuekinger@gmail.com>
2021-12-13 11:23:09 +0800

    服务拆分

---
+++ AuthorizationServer/src/test/java/com/yueking/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yueking.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
